d9411123530969cd14633e5ad87b910eeeecfcdf
Eric tyrrell <eric.tyrrell18+github@gmail.com>
2025-11-05 22:05:45 -0500

    cleanup a little bit

--- main.go
+++ main.go
@@ -9,48 +9,46 @@ import (
   "fmt"
 )
 
-func getOccupancy() ( int, int, error ){
-  scbCount, ptmCount := -1, -1
+func parseLocationCount(locationCode string, body []byte) (int, error) {
+  // yuck
+  pattern := fmt.Sprintf(`'%s' : \{\s*'capacity' : \d+,\s*'count' : (\d+),`, locationCode)
+  regex, err := regexp.Compile(pattern)
+  if err != nil {
+    return -1, err
+  }
+
+  matches := regex.FindStringSubmatch(string(body))
+  if len(matches) > 1 {
+    count, err := strconv.Atoi(matches[1])
+    if err != nil {
+      return -1, err
+    }
+    return count, nil
+  }
+
+  return -1, fmt.Errorf("no count found for %s", locationCode)
+}
+
+func getOccupancy() (int, int, error) {
   resp, err := http.Get("https://portal.rockgympro.com/portal/public/8490bc5e774d0034d09420df23a224b9/occupancy?=&iframeid=occupancyCounter&fId=")
   if err != nil {
-    return ptmCount, scbCount, err
+    return -1, -1, err
   }
   defer resp.Body.Close()
 
   body, err := io.ReadAll(resp.Body)
   if err != nil {
-    return ptmCount, scbCount, err
+    return -1, -1, err
   }
 
-  // Dumb but works
-  ptmRegex, err := regexp.Compile(`'PTM' : \{\s*'capacity' : \d+,\s*'count' : (\d+),`)
-  scbRegex, err := regexp.Compile(`'SCB' : \{\s*'capacity' : \d+,\s*'count' : (\d+),`)
+  ptmCount, ptmErr := parseLocationCount("PTM", body)
+  scbCount, scbErr := parseLocationCount("SCB", body)
 
-  matches := ptmRegex.FindStringSubmatch(string(body))
-  if len(matches) > 1 {
-    count := matches[1]
-
-    i, err := strconv.Atoi(count)
-    if err != nil  {
-      return ptmCount, scbCount, err
-    }
-
-    ptmCount = i;
+  if ptmErr != nil && scbErr != nil {
+    return -1, -1, errors.New("no count found in response")
   }
 
-  matches = scbRegex.FindStringSubmatch(string(body))
-  if len(matches) > 1 {
-    count := matches[1]
-
-    i, err := strconv.Atoi(count)
-    if err != nil  {
-      return ptmCount, scbCount, err
-    }
-
-    scbCount = i;
-  }
-
-  return ptmCount, scbCount, errors.New("no count found in response")
+  return ptmCount, scbCount, nil
 }
 
 func main() {
